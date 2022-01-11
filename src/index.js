c2dd813df3fffcd500d2e571b165fd568c4a960b
nwaguvictor <leofizzy3@gmail.com>
2022-01-11 01:53:12 +0100

    ui setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const socketio = require('./socketio');
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
-const server = createServer(app);
 
+const server = createServer(app);
 socketio(server);
+
 const PORT = process.env.PORT || 2022;
 server.listen(PORT, () => {
   console.log(`::> Server running http://localhost:${PORT}`);
